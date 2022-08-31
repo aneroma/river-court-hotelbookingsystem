807187a9e6453797a7d1b94cb95b8bd9e987b974
Diana Morena <aneroma@yahoo.com>
2022-08-31 16:25:43 +0100

    Modified contact form to use external API.

--- web/src/pages/ContactPage/ContactForm.jsx
+++ web/src/pages/ContactPage/ContactForm.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
 import {v4} from "uuid";
-
-const ContactForm = () => {
-  const formFields = [
-    {name: "name", label: "Your Name", type: "text", required: true},
-    {name: "email", label: "Your Email Address", type: "email", required: true},
-    {name: "message", label: "Your Message", type: "textbox", required: true},
-  ];
-
-  function handleSubmit(e) {
-    e.preventDefault();
+import { useForm, ValidationError } from '@formspree/react';
+function ContactForm() {
+  const [state, handleSubmit] = useForm("myyvzqzz");
+  if (state.succeeded) {
+      return <p>Message has been sent!</p>;
   }
 
-  const className =
-    "p-3 rounded-2xl text-textPrimary placeholder:text-textLight w-full bg-gray-100 focus:outline-primary-400";
+  const formFields = [
+  {name: "name", label: "Your Name", type: "text", required: true},
+  {name: "email", label: "Your Email Address", type: "email", required: true},
+  {name: "message", label: "Your Message", type: "textbox", required: true},
+  ];
+
+  const className = "p-3 rounded-2xl text-textPrimary placeholder:text-textLight w-full bg-gray-100 focus:outline-primary-400";
+
 
   return (
-    <form
+   <form
       action="submit"
       className="flex flex-col bg-white p-4 rounded-3xl gap-4 w-full h-max"
       onSubmit={handleSubmit}
@@ -41,11 +42,16 @@ const ContactForm = () => {
           ></textarea>
         );
       })}
-      <button type="submit" className="btn-primary">
-        Send Message
+      <button type="submit" className="btn-primary" disabled={state.submitting}>
+        Submit
       </button>
     </form>
   );
-};
+}
 
-export default ContactForm;
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
